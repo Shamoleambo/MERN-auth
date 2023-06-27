bf2a44d35e0f309d533126de8c7c5fb924285a36
Tiago Gomes <tidgomes@gmail.com>
2023-06-27 16:41:26 -0300

    feat: use both error handling middlewares in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use('/api/users', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`)
