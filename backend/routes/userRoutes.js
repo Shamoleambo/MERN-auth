e7cf892924a3b75e8b59f78955f8d4413e8b7fdf
Tiago Gomes <tidgomes@gmail.com>
2023-06-27 16:54:25 -0300

    feat: add remaning routes for all the requests

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express'
-import { authUser } from '../controller/userController.js'
+import {
+  registerUser,
+  authUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile
+} from '../controller/userController.js'
 
 const router = express.Router()
 
+router.post('/', registerUser)
 router.post('/auth', authUser)
+router.post('/logout', logoutUser)
+router.route('/profile').get(getUserProfile).put(updateUserProfile)
 
 export default router
