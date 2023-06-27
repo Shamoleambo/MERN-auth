904282ce1c85bd2fe1929270a64508c2b02127c4
Tiago Gomes <tidgomes@gmail.com>
2023-06-27 20:51:36 -0300

    feat: connect the server to the mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
+connectDB()
 const app = express()
 
 app.use('/api/users', userRoutes)
