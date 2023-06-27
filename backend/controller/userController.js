ed6f3e731e0a74bb70dbf5dfd3b521ecb8d3c330
Tiago Gomes <tidgomes@gmail.com>
2023-06-27 16:50:59 -0300

    feat: create dummy controller functions for all the remaning requests

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,3 +3,19 @@ import asyncHandler from 'express-async-handler'
 export const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Auth User' })
 })
+
+export const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Register User' })
+})
+
+export const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout user' })
+})
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get user profile' })
+})
+
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update user Profile' })
+})
