6d350f0c607370ee8055f9f2549c278e568491d3
Tiago Gomes <tidgomes@gmail.com>
2023-06-27 16:26:38 -0300

    feat: wrap authUser in the expressAsyncHandler

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,3 +1,5 @@
-export const authUser = (req, res) => {
+import asyncHandler from 'express-async-handler'
+
+export const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Auth User' })
-}
+})
