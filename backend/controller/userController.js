5736d61524b7271161c26f66e27d3d799ab6c84d
Tiago Gomes <tidgomes@gmail.com>
2023-06-28 15:30:08 -0300

    feat: handle the request to authenticate the user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,7 +3,16 @@ import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
 export const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth User' })
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+  if (user && (await user.matchPassword(password, user.password))) {
+    generateToken(res, user._id)
+    res.status(200).json({ _id: user._id, name: user.name, email: user.email })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
 })
 
 export const registerUser = asyncHandler(async (req, res) => {
