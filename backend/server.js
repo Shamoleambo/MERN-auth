4456c454051ace19974c50c063a00492248f8a56
Tiago Gomes <tidgomes@gmail.com>
2023-06-28 15:36:54 -0300

    feat: parse the incoming requests for the cookie

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
 dotenv.config()
-
-const PORT = process.env.PORT || 3000
-
 connectDB()
+
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
