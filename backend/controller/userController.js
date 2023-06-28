425eda18602c5b3a605bb4bb2268a8363fc5ecc2
Tiago Gomes <tidgomes@gmail.com>
2023-06-28 13:41:51 -0300

    feat: handle the request to register user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,11 +1,26 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
 
 export const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Auth User' })
 })
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Register User' })
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({ name, email, password })
+  if (user) {
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User data')
+  }
 })
 
 export const logoutUser = asyncHandler(async (req, res) => {
