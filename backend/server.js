1b05a71da9ee975b5ded3ee6b2371cff7eb75096
Tiago Gomes <tidgomes@gmail.com>
2023-06-28 13:32:48 -0300

    feat: parse the body for json and urlencoded requests

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 3000
 connectDB()
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/api/users', userRoutes)
 
 app.use(notFound)
