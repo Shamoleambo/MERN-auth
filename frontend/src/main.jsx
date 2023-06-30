72f0807abe5172f0696a6345f7f8765ac6e84b0c
Tiago Gomes <tidgomes@gmail.com>
2023-06-30 13:51:23 -0300

    feat: add Register screen to the React Router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,12 +11,14 @@ import App from './App.jsx'
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
+import RegisterScreen from './screens/RegisterScreen.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route path='/' index={true} element={<HomeScreen />}></Route>
       <Route path='/login' element={<LoginScreen />}></Route>
+      <Route path='/register' element={<RegisterScreen />}></Route>
     </Route>
   )
 )
